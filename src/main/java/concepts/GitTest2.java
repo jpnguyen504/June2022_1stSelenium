7fa33a621b1e6752234b37f1c361b674bfe3fc35
jpnguyen504 <jpnguyen504@gmail.com>
2023-07-26 14:28:05 -0500

    3rd commit

---
+++ src/main/java/concepts/GitTest2.java
@@ -0,0 +1,20 @@
+package concepts;
+
+public class GitTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		printme();
+	}
+	
+	public static void printme() {
+		System.out.println("git test 1");
+	}
+	
+	public static void printme2() {
+		System.out.println("git test 2");
+		System.out.println("git test 3");
+		System.out.println("git test 4");
+		System.out.println("git test 5");
+	}
+}
